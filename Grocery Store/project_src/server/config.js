0b38292f495092ed6a813fa190374b1771b35f09
Akash.Tiwari <Akash.Tiwari@Arcserve.com>
2021-05-23 20:23:27 +0530

    Added Capstone project for grocery store

---
+++ Grocery Store/project_src/server/config.js
@@ -0,0 +1,8 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    "ADD DB LOCALHOST PATH",
+  port: 3030,
+  secret: "WPL_FINAL_PROJECT",
+};
